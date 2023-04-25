937a05c537fbef803f8a81658f345057d7e99554
Mario Nageh <marionageh7@gmail.com>
2023-04-25 09:58:08 -0700

    Fix

--- print_string.c
+++ print_string.c
@@ -9,11 +9,22 @@
 int print_string(va_list args)
 {
 	char *s = va_arg(args, char *);
+	char *begin = s;
+	int i;
 
 	if (s == NULL)
 	{
 		s = "(null)";
+		for (i = 0; i < 6; ++i)
+		{
+			_putchar(s[i]);
+		}
+		return (6);
 	}
 
-	return write(1, s, strlen(s));
+	while (*s)
+	{
+		_putchar(*s++);
+	}
+	return (s - begin);
 }
